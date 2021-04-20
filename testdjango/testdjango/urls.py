d3971ccc60e12d4f2c8f26b5d7f74a441aaaf8e0
AmauriJunior <amaurijr0809@gmail.com>
2021-04-20 18:37:03 -0300

    is new year app

--- testdjango/testdjango/urls.py
+++ testdjango/testdjango/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include("hello.url"))
+    path('hello/', include("hello.url")),
+    path('newyear/', include("newyear.urls"))
 ]
